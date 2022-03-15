b11a7c3983da137be4d6de860788d78f2254bfbb
Guilherme <guilherme.pomp@gmail.com>
2022-03-14 22:34:51 -0300

    Criado estrutura de logs com Elastic Search e Kibana

--- src/ClinicaOnline.Web/Program.cs
+++ src/ClinicaOnline.Web/Program.cs
@@ -1,27 +1,64 @@
+using System;
 using ClinicaOnline.Infrastructure.Data;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace ClinicaOnline.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
+            try
+            {
+                var hostBuilder = CreateHostBuilder(args).Build();                
+                Serilog.Log.Information("Iniciando Web Host");
+                SeedDatabase(hostBuilder);
 
-            SeedDatabase(host);
-
-            host.Run();
+                hostBuilder.Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Serilog.Log.Fatal(ex, "Host encerrado inesperadamente");
+                return 1;
+            }
+            finally
+            {
+                Serilog.Log.CloseAndFlush();
+            }
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-                    WebHost.CreateDefaultBuilder(args)
-                        .UseStartup<Startup>();
-
-        private static void SeedDatabase(IWebHost host)
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var settings = config.Build();
+                    Serilog.Log.Logger = new LoggerConfiguration()
+                        .Enrich.FromLogContext()
+                        .WriteTo.Elasticsearch(
+                            options:
+                                new ElasticsearchSinkOptions(
+                                    new Uri(settings["Elasticsearch:Uri"]))
+                                {
+                                    AutoRegisterTemplate = true,
+                                    AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                                    IndexFormat = "apiclinicaonline-{0:yyyy.MM}"
+                                })
+                        .WriteTo.Console()
+                        .CreateLogger();
+                })
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        
+        private static void SeedDatabase(IHost host)
         {
             using (var scope = host.Services.CreateScope())
             {
