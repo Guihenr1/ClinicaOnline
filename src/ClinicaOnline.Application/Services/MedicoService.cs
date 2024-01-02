f94e9758eb13cd08cf70827e1d8eb2c139eaa8a5
Guilherme Pompilio <guilherme.pomp@gmail.com>
2024-01-02 20:29:06 -0300

    Azure function to validate doctor speciality

--- src/ClinicaOnline.Application/Services/MedicoService.cs
+++ src/ClinicaOnline.Application/Services/MedicoService.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using ClinicaOnline.Application.Interfaces;
 using ClinicaOnline.Application.Mapper;
@@ -9,6 +11,8 @@ using ClinicaOnline.Application.Models.Response;
 using ClinicaOnline.Core.Entities;
 using ClinicaOnline.Core.Notification;
 using ClinicaOnline.Core.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ClinicaOnline.Application.Services
 {
@@ -17,11 +21,13 @@ namespace ClinicaOnline.Application.Services
         private IMedicoRepository _medicoRepository;
         private Lazy<IPacienteService> _pacienteService;
         private NotificationContext _notificationContext;
-        public MedicoService(IMedicoRepository medicoRepository, Lazy<IPacienteService> pacienteService, NotificationContext notificationContext)
+        private readonly IConfiguration _configuration;
+        public MedicoService(IMedicoRepository medicoRepository, Lazy<IPacienteService> pacienteService, NotificationContext notificationContext, IConfiguration configuration)
         {
             _medicoRepository = medicoRepository;
             _pacienteService = pacienteService;
             _notificationContext = notificationContext;
+            _configuration = configuration;
         }
 
         public async Task<IReadOnlyList<Medico>> GetAll()
@@ -34,14 +40,21 @@ namespace ClinicaOnline.Application.Services
             var checkMedicoExist = await _medicoRepository.GetById(id);
             if (checkMedicoExist == null)
             {
-			    _notificationContext.AddNotification(Guid.NewGuid().ToString(), "Médico não encontrado");
+                _notificationContext.AddNotification(Guid.NewGuid().ToString(), "Médico não encontrado");
                 return;
             }
 
             var checkCrmAndUfCrm = await _medicoRepository.GetByCrmAndUfCrm(model.Crm, model.UfCrm);
             if (checkCrmAndUfCrm != null && checkCrmAndUfCrm.Id != id)
             {
-			    _notificationContext.AddNotification(Guid.NewGuid().ToString(), "CRM e Uf já cadastrados para outro médico");
+                _notificationContext.AddNotification(Guid.NewGuid().ToString(), "CRM e Uf já cadastrados para outro médico");
+                return;
+            }
+
+            var checkSpeciality = await ValidateDoctorSpeciality(model.Especialidade);
+            if (!checkSpeciality)
+            {
+                _notificationContext.AddNotification(Guid.NewGuid().ToString(), "Especialidade inválida");
                 return;
             }
 
@@ -56,8 +69,15 @@ namespace ClinicaOnline.Application.Services
 
             var checkCrmAndUfCrm = await _medicoRepository.GetByCrmAndUfCrm(medico.Crm, medico.UfCrm);
             if (checkCrmAndUfCrm != null)
-            {                
-			    _notificationContext.AddNotification(Guid.NewGuid().ToString(), "CRM e Uf CRM já cadastrados");
+            {
+                _notificationContext.AddNotification(Guid.NewGuid().ToString(), "CRM e Uf CRM já cadastrados");
+                return response;
+            }
+
+            var checkSpeciality = await ValidateDoctorSpeciality(medico.Especialidade);
+            if (!checkSpeciality)
+            {
+                _notificationContext.AddNotification(Guid.NewGuid().ToString(), "Especialidade inválida");
                 return response;
             }
 
@@ -73,17 +93,17 @@ namespace ClinicaOnline.Application.Services
             var pacientes = await _pacienteService.Value.GetPacientesByMedicoId(medicoId);
             if (pacientes.Any())
             {
-			    _notificationContext.AddNotification(Guid.NewGuid().ToString(), "Não é possível excluir médicos com pacientes associados");
+                _notificationContext.AddNotification(Guid.NewGuid().ToString(), "Não é possível excluir médicos com pacientes associados");
                 return;
             }
 
             var medico = await _medicoRepository.GetById(medicoId);
             if (medico == null)
             {
-			    _notificationContext.AddNotification(Guid.NewGuid().ToString(), "Médico não encontrado");
+                _notificationContext.AddNotification(Guid.NewGuid().ToString(), "Médico não encontrado");
                 return;
             }
-            
+
             await _medicoRepository.Delete(medico);
         }
 
@@ -100,6 +120,24 @@ namespace ClinicaOnline.Application.Services
                 medicos = medicos.Where(x => x.UfCrm.ToLower() == ufCrm.ToLower()).ToList();
 
             return medicos;
+        }
+
+        private async Task<bool> ValidateDoctorSpeciality(string doctorSpeciality)
+        {
+            var isValid = "false";
+            var functionUrl = _configuration.GetSection("AzureFunctions")["ValidateDoctorSpeciality"];
+            var functionKey = _configuration.GetSection("AzureFunctions")["ValidateDoctorSpecialityKey"];
+
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("x-functions-key", functionKey);
+            var response = await client.PostAsJsonAsync(functionUrl, new { doctorSpeciality });
+
+            if (response.IsSuccessStatusCode)
+            {
+                isValid = await response.Content.ReadAsStringAsync();
+            }
+
+            return Convert.ToBoolean(isValid);
         }
     }
 }
